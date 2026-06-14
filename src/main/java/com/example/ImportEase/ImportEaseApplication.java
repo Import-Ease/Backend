c863624d5820821f897bac2714a94a271a3f90d2
SiLentPr0t0c0l <esuonia4u@gmail.com>
2026-06-14 15:26:21 +0000

    feat: Database setup

---
+++ src/main/java/com/example/ImportEase/ImportEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImportEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImportEase/ImportEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImportEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
