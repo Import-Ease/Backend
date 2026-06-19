d580e1883ce3600b88fd5cf9b22b77a0c20f4e52
Bernita-07 <akosuabernita@gmail.com>
2026-06-19 03:51:32 +0000

    Created ProductRepository and SupplierRepository

---
+++ src/main/java/com/example/importease/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.importease.repository;
+
+import com.example.importease.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
